be2dc184e9d3bf3e74864354660419305475ebfe
Anna Polyakova <polyakova.as@mail.ru>
2022-10-16 20:18:36 +0300

    homework for lesson-2

---
+++ timer.js
@@ -0,0 +1,43 @@
+//formaat hh-DD-MM-YYYY
+require('moment-precise-range-plugin');
+const moment = require ('moment');
+const EventEmitter = require ('events');
+const [ dateStringInFuture ] = process.argv.slice(2);
+const DATE_FORMAT_PATTERN = 'YYYY-MM-DD HH:mm:ss';
+
+const getDateFromString = (dateString) => {
+    const [ hour, day, month, year ] = dateString.split('-');
+
+    return new Date(Date.UTC(year, month - 1, day, hour));
+};
+
+const showTimer = (dateInFuture) => {
+    const dateNow = new Date();
+
+    if (dateNow >= dateInFuture) {
+        emitter.emit('timerEnd'); 
+    } else {
+        const currentDateFormatted = moment(dateNow, DATE_FORMAT_PATTERN);
+        const futureDateFormatted = moment(dateInFuture, DATE_FORMAT_PATTERN);
+        const diff = moment.preciseDiff(currentDateFormatted, futureDateFormatted);
+
+        console.clear;
+        console.log(diff);
+    }
+};
+
+const showTimerDone = (timerId) => {
+    clearInterval(timerId);
+    console.log('Таймер истек');
+}
+
+const emitter = new EventEmitter();
+const dateInFuture = getDateFromString(dateStringInFuture);
+const timerId = setInterval(() => {
+    emitter.emit('timerTick', dateInFuture);
+}, 1000)
+
+emitter.on('timerTick', showTimer);
+emitter.on('timerEnd', () => {
+    showTimerDone(timerId);
+});
